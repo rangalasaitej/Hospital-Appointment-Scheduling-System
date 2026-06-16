03b695961a72cd9a3a7f29950c8f5b30bf0c6bd5
rangalasaitej <rangalasaitej@klh.edu.in>
2026-06-16 11:02:00 +0530

    Delete Hospital-Appointment-Scheduling-System directory

--- Hospital-Appointment-Scheduling-System/frontend/src/pages/candidate/Availability.jsx
+++
@@ -1,4 +0,0 @@
-import AvailabilityPage from '../../components/shared/AvailabilityPage';
-export default function CandidateAvailability() {
-  return <AvailabilityPage role="candidate" />;
-}
--- Hospital-Appointment-Scheduling-System/frontend/src/pages/interviewer/Availability.jsx
+++
@@ -1,4 +0,0 @@
-import AvailabilityPage from '../../components/shared/AvailabilityPage';
-export default function InterviewerAvailability() {
-  return <AvailabilityPage role="interviewer" />;
-}
